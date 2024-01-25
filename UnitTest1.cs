39767aef7c20886cb87d8a7c15070b296f4df720
ChhayaPatil12 <patilchhaya1507@gmail.com>
2024-01-25 22:15:45 +0530

    Test Cases

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,6 +8,7 @@ namespace ToyRobotStimulationTest
     [TestClass]
     public class UnitTest1
     {
+        // Test to ensure that the robot moves correctly on the tabletop
         [TestMethod]
         public void TestRobotMovement()
         {
@@ -21,24 +22,27 @@ namespace ToyRobotStimulationTest
             Assert.AreEqual("0,1,NORTH", result);
         }
 
+        // Test to check the robot's ability to rotate left and right correctly
         [TestMethod]
         public void TestRobotRotation()
         {
             Tabletop tabletop = new Tabletop();
             Robot robot = new Robot(tabletop);
 
+            // Rotate left
             robot.Place(0, 0, "NORTH");
             robot.Left();
             string resultLeft = robot.Report();
+            Assert.AreEqual("0,0,WEST", resultLeft);
 
+            // Rotate right
             robot.Place(0, 0, "NORTH");
             robot.Right();
             string resultRight = robot.Report();
-
-            Assert.AreEqual("0,0,WEST", resultLeft);
             Assert.AreEqual("0,0,EAST", resultRight);
         }
 
+        // Test to check the integration of multiple commands
         [TestMethod]
         public void TestRobotIntegration()
         {
@@ -55,6 +59,67 @@ namespace ToyRobotStimulationTest
             Assert.AreEqual("0,1,NORTH", result);
         }
 
+        // Test to check that the robot ignores commands when not placed on the tabletop
+        //[TestMethod]
+        //public void TestRobotIgnoredCommands()
+        //{
+        //    Tabletop tabletop = new Tabletop();
+        //    Robot robot = new Robot(tabletop);
+
+        //    // Trying to move, rotate, and report before placing the robot
+        //    robot.Move();
+        //    robot.Left();
+        //    robot.Right();
+        //    string resultBeforePlacement = robot.Report();
+
+        //    // Place the robot and then check if the commands are executed
+        //    robot.Place(2, 2, "EAST");
+        //    robot.Move();
+        //    robot.Left();
+        //    robot.Right();
+        //    string resultAfterPlacement = robot.Report();
+
+        //    Assert.AreEqual("2,2,EAST", resultBeforePlacement);
+        //    Assert.AreEqual("3,2,EAST", resultAfterPlacement);
+        //}
+
+        // Test to check that the robot ignores invalid PLACE commands
+        //[TestMethod]
+        //public void TestInvalidPlaceCommand()
+        //{
+        //    Tabletop tabletop = new Tabletop();
+        //    Robot robot = new Robot(tabletop);
+
+        //    // Invalid place command with incorrect arguments
+        //    robot.Place(1, 1, "NORTH");
+        //    robot.Place(1, 1, "INVALID_DIRECTION");
+        //    string resultInvalidPlace = robot.Report();
+
+        //    // Valid place command
+        //    robot.Place(2, 2, "SOUTH");
+        //    string resultValidPlace = robot.Report();
+
+        //    Assert.AreEqual("1,1,NORTH", resultInvalidPlace);
+        //    Assert.AreEqual("2,2,SOUTH", resultValidPlace);
+        //}
+
+        // Test to check that the robot ignores moves that would make it fall off the tabletop
+        [TestMethod]
+        public void TestAvoidFallingOffTabletop()
+        {
+            Tabletop tabletop = new Tabletop();
+            Robot robot = new Robot(tabletop);
+
+            // Place the robot at the edge of the tabletop facing NORTH
+            robot.Place(0, 4, "NORTH");
+
+            // Try to move, but it should not fall off the tabletop
+            robot.Move();
+            robot.Move();
+            string result = robot.Report();
+
+            Assert.AreEqual("0,4,NORTH", result);
+        }
 
         private void ExecuteCommands(Robot robot, string[] commands, out string result)
         {
@@ -96,4 +161,3 @@ namespace ToyRobotStimulationTest
         }
     }
 }
-  
